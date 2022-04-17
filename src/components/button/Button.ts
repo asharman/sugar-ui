e967d99bfe9ec89866cff0ec16b5339656a7c250
Austin Sharman <asharman22@gmail.com>
2022-04-17 17:58:09 -0400

    clean up stories

--- src/components/button/Button.ts
+++ src/components/button/Button.ts
@@ -1,13 +1,10 @@
 import { LitElement, html, css } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import styles from './Button.styles'
+import styles from "./Button.styles";
 
 @customElement("sui-button")
 export class Button extends LitElement {
   static styles = styles;
-
-  @property({ type: String })
-  variant = 'primary'
 
   render() {
     return html`
--- src/components/button/stories/Button.stories.ts
+++ src/components/button/stories/Button.stories.ts
@@ -1,10 +1,22 @@
 import { html } from "lit";
+import { ifDefined } from "lit-html/directives/if-defined";
 import "../Button";
 
 export default {
   title: "Example/Button",
 };
 
-export const Default = () => html`<sui-button>Buy Now</sui-button>`;
+const Template = ({ variant, text }) =>
+  html`<sui-button variant=${ifDefined(variant)}>${text}</sui-button>`;
 
-export const VariantNegative = () => html`<sui-button variant="negative">Cancel</sui-button>`
+//👇 Each story then reuses that template
+export const Default = Template.bind({});
+Default.args = {
+  text: "Buy Now",
+};
+
+export const VariantNegative = Template.bind({});
+VariantNegative.args = {
+  variant: "negative",
+  text: "Cancel",
+};
